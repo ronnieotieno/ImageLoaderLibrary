066885f2fe1221d67ce3f4ede5a0507c4c003feb
ronnieotieno <ronnieotieno2@gmail.com>
2020-01-03 19:05:34 +0300

    Added Cache and Builder

--- imageloaderlibrary/src/main/java/dev/ronnieotieno/imageloaderlibrary/ImageLoader.kt
+++ imageloaderlibrary/src/main/java/dev/ronnieotieno/imageloaderlibrary/ImageLoader.kt
@@ -1,37 +1,102 @@
 package dev.ronnieotieno.imageloaderlibrary
 
+import android.content.ComponentCallbacks2
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.LruCache
 import android.widget.ImageView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.net.URL
 
 
-class ImageLoader {
+class ImageLoader : ComponentCallbacks2 {
 
-    val memcache = MemoryCache()
+    // private lateinit var memoryCache2: LruCache<String, Bitmap>
+    private lateinit var imageUrl: String
+    private lateinit var imageView: ImageView
 
-    fun load(imageUrl: String, imageView: ImageView) {
-        CoroutineScope(IO).launch {
-            val bitmap: Bitmap? = memcache[imageUrl]
-            if (bitmap != null) {
-                withContext(Dispatchers.Main) {
-                    imageView.setImageBitmap(bitmap)
-                }
-            } else {
-                val url = URL(imageUrl)
-                val image = BitmapFactory.decodeStream(url.openConnection().getInputStream())
-                memcache.put(imageUrl, image)
 
-                withContext(Dispatchers.Main) {
-                    imageView.setImageBitmap(image)
-                }
+    lateinit var memoryCache: LruCache<String, Bitmap>
+    private lateinit var job: Job
+    private val _temp = ImageLoader()
+
+    fun load(image: String): ImageLoader {
+        _temp.imageUrl = image
+        return this
+    }
+
+
+    fun into(imageView: ImageView): ImageLoader {
+        _temp.imageView = imageView
+        load2(imageUrl,imageView)
+        return this
+    }
+
+
+    fun load2(imageUrl: String, imageView: ImageView): ImageLoader {
+
+        loadImage(imageUrl, imageView)
+
+        return this
+    }
+
+
+    fun loadImage(imageUrl: String, imageView: ImageView) {
+
+        val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
+
+        val cacheSize = maxMemory / 8
+
+        memoryCache = object : LruCache<String, Bitmap>(cacheSize) {
+
+            override fun sizeOf(key: String, bitmap: Bitmap): Int {
+
+                return bitmap.byteCount / 1024
             }
+        }
+
+        job = CoroutineScope(IO).launch {
+
+            loadBitmap(imageUrl, imageView)
+
+        }
+
+    }
+
+    suspend fun loadBitmap(imageUrl: String, imageView: ImageView) {
+
+        val bitmap: Bitmap? = memoryCache[imageUrl]
+
+        if (bitmap != null) {
+            withContext(Dispatchers.Main) {
+                imageView.setImageBitmap(bitmap)
+            }
+        } else {
+            val url = URL(imageUrl)
+            val image = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+
+            withContext(Dispatchers.Main) {
+                imageView.setImageBitmap(image)
+            }
+            memoryCache.put(imageUrl, image)
         }
     }
 
+    override fun onLowMemory() {
+
+    }
+
+    override fun onConfigurationChanged(p0: Configuration) {
+
+    }
+
+    override fun onTrimMemory(level: Int) {
+        if (level >= ComponentCallbacks2.TRIM_MEMORY_MODERATE) {
+            memoryCache.evictAll()
+        } else if (level >= ComponentCallbacks2.TRIM_MEMORY_BACKGROUND) {
+            memoryCache.trimToSize(memoryCache.size() / 2)
+        }
+    }
 }
